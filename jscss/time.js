e75746d18d2f98e937f0331a6353eee97b98edb5
xiefeng601 <73860897+xiefeng601@users.noreply.github.com>
2022-12-07 20:33:08 +0800

    Update time.js

--- jscss/time.js
+++ jscss/time.js
@@ -13,8 +13,8 @@ function showTime() {
 	minutes = "0" + minutes; 
 	if (seconds < 10) 
 	seconds = "0" + seconds; 
-	var currentTimeString = "Dear Xiao:<br>"
-		+ "I have been in love with you for（QQ小草已经:<br> "
+	var currentTimeString = "Dear ***:<br>"
+		+ "*********************************:<br> "
 		+ "&nbsp&nbsp&nbsp&nbsp&nbsp"
 		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + days +" </c>day " 
 		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + hours+" </c>hour "
